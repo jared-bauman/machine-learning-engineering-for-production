09d72d31946f9e4129fcb578f6dfcb72efde6929
jared-bauman <jaredjbauman@gmail.com>
2023-09-28 10:07:59 -0700

    Testing access token

--- course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
+++ course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
@@ -12,7 +12,7 @@ app = FastAPI(title="Predicting Wine Class with batching")
 with open("models/wine.pkl", "rb") as file:
     clf = pickle.load(file)
 
-# min_items seems to make this break
+# min_items seems to make this break. Could break...
 class Wine(BaseModel):
     batches: List[conlist(item_type=float, min_items=13, max_items=13)]
 
